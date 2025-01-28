7242119dc9d3d3451aec9da5f36c26873ae73428
RudraSuthar09 <rudrasuthar09@gmail.com>
2025-01-28 19:01:34 +0530

    frontend completed

--- Frontend/src/Pages/Cart/Cart.jsx
+++ Frontend/src/Pages/Cart/Cart.jsx
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react'
 import './Cart.css'
 import { StoreContext } from '../../context/StoreContext'
+import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
 
-  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart,getTotalCartAmount } = useContext(StoreContext);
+  const navigate  =useNavigate();
 
   return (
     <div className='cart'>
@@ -19,26 +21,59 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map((item,index)=>{
-          if(cartItems[item.id]>0){
-            return(
+        {food_list.map((item, index) => {
+          if (cartItems[item.id] > 0) {
+            return (
               <div>
-              <div className='cart-items-title cart-items-item '>
-                <img src={item.image} alt="" />
-                <p>{item.name}</p>
-                <p>{item.price}</p>
-                <p>{cartItems[item.id]}</p>
-                <p>{item.price*cartItems[item.id]} </p>
-                <p onClick={()=>removeFromCart(item.id)} className='cross'>x</p>
+                <div className='cart-items-title cart-items-item '>
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item.id]}</p>
+                  <p>${item.price * cartItems[item.id]} </p>
+                  <p onClick={() => removeFromCart(item.id)} className='cross'>x</p>
+                </div>
+                <hr />
               </div>
-              <hr />
-              </div>
-              
+
             )
           }
 
         })}
       </div>
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details"><p>Delivery fee</p>
+              <p> ${getTotalCartAmount()===0?0:2}</p></div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+            </div>
+          </div>
+
+          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
+
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a promo code ,enter it here</p>
+            <div className='cart-promocode-input'>
+              <input type="text" placeholder='promo-code' />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
     </div>
   )
 }
--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -6,21 +6,29 @@ const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
 
   const addToCart = (itemId) => {
-    if(!cartItems[itemId]){
-      setCartItems((prev)=>({...prev,[itemId]:1}))
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
     }
-    else{
-      setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+    else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
     }
   };
 
   const removeFromCart = (itemId) => {
-      setCartItems((prev)=> ({...prev,[itemId]:prev[itemId]-1}))
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
   };
 
-  useEffect(() => {
-    console.log(cartItems);
-  }, [cartItems]);
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product.id === item);
+        totalAmount += itemInfo.price * cartItems[item]
+      }
+
+    }
+    return totalAmount;
+  }
 
   const contextValue = {
     food_list,
@@ -28,6 +36,7 @@ const StoreContextProvider = (props) => {
     setCartItems,
     addToCart,
     removeFromCart,
+    getTotalCartAmount
   };
 
   return (
