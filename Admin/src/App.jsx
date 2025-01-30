976c31a35a2bf12639d6550d274772d85df33e70
Suhanii054 <suhaniach25@gmail.com>
2025-01-30 14:37:43 +0530

    database

---
+++ Admin/src/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar '
+import Sidebar from './components/Sidebar/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import { Orders } from './Pages/Orders/Orders'
+import List from './Pages/List/List'
+import Add from './Pages/Add/add'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+        <Routes>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/list" element={<List/>}/>
+          <Route path="/orders" element={<Orders/>}/>
+
+        </Routes>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
