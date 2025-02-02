dcb5cb241a333df74af3b1cd0caa63795d1d755b
Suhanii054 <suhaniach25@gmail.com>
2025-02-02 17:03:32 +0530

    fetching image form database

--- Frontend/src/context/StoreContext.jsx
+++ Frontend/src/context/StoreContext.jsx
@@ -1,11 +1,13 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/frontend_assets/assets";
+// import { food_list } from "../assets/frontend_assets/assets";
 export const StoreContext = createContext(null);
+import axios from "axios";
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const url = "http://localhost:4000";
   const [token, setToken] = useState(localStorage.getItem("token") || ""); // Load token on app start
+  const[food_list, setFoodList]= useState([]);
 
   useEffect(() => {
     if (token) {
@@ -35,6 +37,24 @@ const StoreContextProvider = (props) => {
     }
     return totalAmount;
   };
+
+  const fetchFoodList= async()=>{
+    const response= await axios.get(url+"/api/food/list");
+    setFoodList(response.data.data);
+    
+  }
+
+  useEffect(()=>{
+  
+    async function loadData(){
+      await fetchFoodList()
+      if(localStorage.getItem("token")){
+        setToken(localStorage.getItem("token"));
+      }
+
+    }
+    loadData();
+  },[])
 
   const contextValue = {
     food_list,
