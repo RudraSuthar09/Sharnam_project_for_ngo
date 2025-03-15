ba45619939a8de139916b52a74b7c4466670fa86
RudraSuthar09 <rudrasuthar09@gmail.com>
2025-03-15 14:45:33 +0530

    changes

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState } from 'react';
 import './Navbar.css';
 import { assets } from '../../assets/frontend_assets/assets';
 import { Link, useNavigate } from 'react-router-dom';
@@ -7,37 +7,38 @@ import { StoreContext } from '../../context/StoreContext';
 const Navbar = ({ setShowLogin }) => {
     const [menu, setMenu] = useState("home");
     const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
+    const navigate = useNavigate();
 
-    // // Fetch token from localStorage when component mounts
-    // useEffect(() => {
-    //     const storedToken = localStorage.getItem("token");
-    //     if (storedToken) {
-    //         setToken(storedToken);
-    //     }
-    // }, []);
-    const navigate= useNavigate();
-
-    const logout=()=>{
+    const logout = () => {
         localStorage.removeItem("token");
         setToken("");
         navigate("/");
-
-    }
+    };
 
     return (
         <div className="Navbar">
-            <Link to='/shopping'> <img src={assets.logo} alt="Logo" className="logo" /></Link>
+            {/* Tomato Logo + Sharanam */}
+            <div className="navbar-logo-container">
+                <Link to='/shopping'>
+                    <img src={assets.logo} alt="Logo" className="logo" />
+                </Link>
+                <Link to='/' className="sharanam-text">
+                    <h1>Sharanam</h1>
+                </Link>
+            </div>
+
             <ul className="navbar-menu">
                 <Link to='/' className={menu === "home" ? "active" : ""} onClick={() => setMenu("home")}>Home</Link>
                 <a href='#explore-menu' className={menu === "menu" ? "active" : ""} onClick={() => setMenu("menu")}>Menu</a>
                 <a href='#app-download' className={menu === "mobile-app" ? "active" : ""} onClick={() => setMenu("mobile-app")}>Mobile App</a>
                 <a href='#footer' className={menu === "Contact-us" ? "active" : ""} onClick={() => setMenu("Contact-us")}>Contact</a>
             </ul>
+
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="Search Icon" className="search-icon" />
                 <div className="navbar-search-icon">
-                <Link to='/shopping/cart'> <img src={assets.basket_icon} alt="Basket Icon" /></Link>
-                <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
+                    <Link to='/shopping/cart'> <img src={assets.basket_icon} alt="Basket Icon" /></Link>
+                    <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
                 </div>
                 {!token ? (
                     <button onClick={() => setShowLogin(true)}>Sign In</button>
